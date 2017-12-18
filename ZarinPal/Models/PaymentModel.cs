a373cf04dc6259e1b8d7bc6f578a9af9bc88d679
vira1368 <vira1368@gmail.com>
2017-12-18 18:26:50 +0330

    Initial commit

---
+++ ZarinPal/Models/PaymentModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZarinPal.Models
+{
+    public class PaymentModel
+    {
+        [Display(Name = "کد سفارش")]
+        public int OrderId { get; set; }
+        [Display(Name = "قیمت")]
+        [Required(ErrorMessage = "لطفاً قیمت را وارد نمائید.")]
+        public int Amount { get; set; }
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفاً توضیحات را وارد نمائید.")]
+        [MinLength(2, ErrorMessage = "توضیحات باید حداقل 2 کاراکتر باشد.")]
+        public string Description { get; set; }
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Display(Name = "موبایل")]
+        public string Mobile { get; set; }
+    }
+}
